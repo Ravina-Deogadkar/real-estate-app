9639fd6a14237a91f1c6450ddf81523fcc6498aa
Ravina Deogadkar <ravina.deogadkar@cctech.co.in>
2020-05-07 18:00:32 +0530

    Feat: modified dashboard display code

--- src/templates/show-adds.js
+++ src/templates/show-adds.js
@@ -1,99 +1,97 @@
-import React, { Component } from 'react';
-import Grid from '@material-ui/core/Grid';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import { getAdsAction } from '../store/action/ads-action';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { getAdsAction } from "../store/action/ads-action";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardImage from "../image/Ads.jpg";
 
 const useStyles = (theme) => ({
-	root: {
-		display: "flex",
-		marginLeft: "10%",
-	},
-	card: {
-		padding: "5%"
-	}
+  root: {
+    display: "flex",
+    marginLeft: "10%",
+  },
+  card: {
+    padding: "5%",
+  },
 });
 class ShowAdds extends Component {
-	constructor(props) {
-		super(props);
-		console.log("show details");
-		// var AdsData;
-		// this.state={
-		//     adsDetails:{
+  constructor(props) {
+    super(props);
+    console.log("show details");
+    // var AdsData;
+    // this.state={
+    //     adsDetails:{
 
-		//     }
-		// }
-		// console.log(this.state);
-	}
+    //     }
+    // }
+    // console.log(this.state);
+  }
 
-	render() {
-		const { classes, getAdsAction_action, adsDetails } = this.props;
-		getAdsAction_action();
-		// this.AdsData = this.props;
-		console.log(adsDetails);
-		return (
-			<React.Fragment>
-				<h2>Short Stay</h2>
-				<div className={classes.root}>
+  componentDidMount() {
+    this.props.getAdsAction_action();
+    console.log(this.props);
+  }
+  render() {
+    const { classes, adsDetails } = this.props;
+    // this.AdsData = this.props;
+    console.log(adsDetails);
+    return (
+      <React.Fragment>
+        <h2>Short Stay</h2>
+        <div className={classes.root}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Card className={classes.card}>
+                <CardHeader title={adsDetails.title} />
+                <CardMedia
+                  className={classes.media}
+                  image={CardImage}
+                  title="Ads"
+                />
 
-					<Grid container spacing={3}>
-						<Grid item xs={12}>
-							<Card className={classes.card}>
-
-
-								<CardHeader
-									title={adsDetails.title}
-								/>
-								<CardMedia
-									className={classes.media}
-									image="../src/image/Ads.jpg"
-									title="Ads"
-								/>
-
-								<CardContent>
-									
-									<Typography variant="h5" component="h2">
-										{adsDetails.housetype}
-									</Typography>
-									<Typography className={classes.pos} color="textSecondary">
-										{adsDetails.price}
-									</Typography>
-									<Typography variant="body2" component="p">
-										{adsDetails.location}
-									</Typography>
-								</CardContent>
-
-							</Card>
-						</Grid>
-
-					</Grid>
-				</div>
-			</React.Fragment>
-		);
-	}
+                <CardContent>
+                  <Typography variant="h5" component="h2">
+                    {adsDetails.housetype}
+                  </Typography>
+                  <Typography className={classes.pos} color="textSecondary">
+                    {adsDetails.price}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    {adsDetails.location}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 ShowAdds.propTypes = {
-	adsDetails: PropTypes.object,
-	getAdsAction_action: PropTypes.func
-}
+  adsDetails: PropTypes.object,
+  getAdsAction_action: PropTypes.func,
+};
 const mapStateToProps = (state) => {
-	return {
-		adsDetails: state.adsDetails
-	}
-}
+  return {
+    adsDetails: state.adsDetails,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
-	return {
-		getAdsAction_action: () => dispatch(getAdsAction())
-	}
-}
+  return {
+    getAdsAction_action: () => dispatch(getAdsAction()),
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(useStyles)(ShowAdds))
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(useStyles)(ShowAdds));

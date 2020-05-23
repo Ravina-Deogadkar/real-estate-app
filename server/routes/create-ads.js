3d447a281eaf669c387e0dc48835b3b4ef902725
Ravina Deogadkar <ravina.deogadkar@cctech.co.in>
2020-05-23 19:05:52 +0530

    chore: removed unwanted data

--- server/routes/create-ads.js
+++ server/routes/create-ads.js
@@ -1,30 +1,29 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var adsData = require('../data/ads');
+var adsData = require("../data/ads");
 
 /* GET ads listing. */
-router.get('/', (req, res, next ) => {
-	//try {
-	// 	//if (adsData !== undefined && adsData.length !== 0)
-	// 		res.sendstatus(200);
-	// 	//else
-	// 	//	res.status(500).send("No ads data found");
-	// } catch (ex) {
-	// 	res.sendstatus(500);
-	// }
+router.get("/", (req, res, next) => {
+  //try {
+  // 	//if (adsData !== undefined && adsData.length !== 0)
+  // 		res.sendstatus(200);
+  // 	//else
+  // 	//	res.status(500).send("No ads data found");
+  // } catch (ex) {
+  // 	res.sendstatus(500);
+  // }
 
-	//res.send("Hello dataa ");
-	console.log(req.query);
+  //res.send("Hello dataa ");
+  //console.log(req.query);
 
-	for (const key in req.query) {
-		console.log(key, req.query[key])
-	  }
-	res.send(adsData);
+  // for (const key in req.query) {
+  // 	console.log(key, req.query[key])
+  // }
+  res.send(adsData);
 });
 
-
-router.post('/create', function (req, res, next) {
-	res.send('ads successfully created');
+router.post("/create", function (req, res, next) {
+  res.send("ads successfully created");
 });
 module.exports = router;

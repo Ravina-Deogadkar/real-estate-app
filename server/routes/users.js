5e81ec2b8ac1c43a387730ed728ce9f0f2a6ba6b
Ravina Deogadkar <ravina.deogadkar@cctech.co.in>
2020-06-06 17:37:01 +0530

    Feat: adds dummy api endpoint for sign in and sign up

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.get("/register", function (res, req) {
+  res.send("registered successfully.");
+});
+router.get("/login/:username", function (res, req) {
+  res.send("loged in successfully.");
 });
 
 module.exports = router;

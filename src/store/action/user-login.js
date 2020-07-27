e99fcb307f4800b85e2510ead2d4a4eca080422d
Ravina Deogadkar <ravina.deogadkar@cctech.co.in>
2020-07-27 17:38:21 +0530

    Feat: adds logout code on login page

--- src/store/action/user-login.js
+++ src/store/action/user-login.js
@@ -8,11 +8,27 @@ export const verifyUser = (data) => {
   };
 };
 
+export const signoutUser = (data) => {
+  return {
+    type: "SIGN_OUT",
+    value: data,
+  };
+};
+
 export const verifyUserAction = (data) => {
   const url = config.HostURL + "/users";
   console.log("Successful");
   data.loginDetails["Loginstatus"] = true;
   return (dispatch) => {
     dispatch(verifyUser(data));
+  };
+};
+
+export const signoutUserAction = (data) => {
+  const url = config.HostURL + "/users";
+  console.log("Successful");
+  data.loginDetails["Loginstatus"] = false;
+  return (dispatch) => {
+    dispatch(signoutUser(data));
   };
 };

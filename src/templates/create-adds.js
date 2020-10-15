b6185af3a1315d791e42b2eeaeb262c5b0a98039
Ravina Deogadkar <ravina.deogadkar@cctech.co.in>
2020-10-15 15:58:54 +0530

    Style: added styling to create-adds page

--- src/templates/create-adds.js
+++ src/templates/create-adds.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import { withStyles } from "@material-ui/core/styles";
@@ -36,6 +36,32 @@ const useStyles = (theme) => ({
     width: "500px",
     margin: "auto",
   },
+  header: {
+    color: "rgba(63, 81, 181, 1)",
+    boxShadow: "0px 4px 3px rgba(202, 203, 210, 1)",
+    padding: "10px",
+    fontSize: "large",
+    width: "100%",
+    //flexGrow: 0,
+    // flexBasis: "80%",
+  },
+  headerCover: {
+    display: "flex",
+    justifyContent: "space-between",
+    [theme.breakpoints.down("sm") && theme.breakpoints.up("xs")]: {
+      marginTop: "80px",
+    },
+
+    [theme.breakpoints.down("md") && theme.breakpoints.up("sm")]: {
+      marginTop: "10%",
+    },
+    [theme.breakpoints.up("md")]: {
+      marginTop: "2%",
+    },
+    [theme.breakpoints.up("lg")]: {
+      marginTop: "0",
+    },
+  },
 });
 
 class CreateAdds extends Component {
@@ -65,147 +91,127 @@ class CreateAdds extends Component {
     this.props.createAdsAction_action(this.state);
   };
   render() {
-    const classes = this.props;
+    const { classes } = this.props;
+    console.log("style class", classes);
     return (
-      // <div className={classes.root}>
-      // <Layout>
-      <div
-        style={{
-          width: "60%",
-          flexGrow: 1,
-          padding: "3%",
-        }}
-        className={classes.content}
-      >
+      <Fragment>
         <div className={classes.toolbar} />
-        <h1>Create Ads</h1>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <TextField
-              id="outlined-name"
-              label="Title"
-              fullWidth
-              className={classes.textField}
-              value={this.state.adsDetails.title}
-              onChange={this.handleChange("title")}
-              margin="normal"
-            />
-          </Grid>
+        <div className={classes.headerCover}>
+          <div className={classes.header}>Create Ads</div>
+        </div>
+        <div
+          style={{
+            width: "50%",
+            flexGrow: 1,
+            padding: "3%",
+          }}
+          className={classes.content}
+        >
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <TextField
+                id="outlined-name"
+                label="Title"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                fullWidth
+                className={classes.textField}
+                value={this.state.adsDetails.title}
+                onChange={this.handleChange("title")}
+                margin="normal"
+              />
+            </Grid>
 
-          <Grid item xs={6}>
-            <TextField
-              id="standard-multiline-flexible"
-              label="Price"
-              fullWidth
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              value={this.state.adsDetails.price}
-              onChange={this.handleChange("price")}
-              className={classes.textField}
-              margin="normal"
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              id="standard-multiline-flexible"
-              label="Floor Space"
-              fullWidth
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              value={this.state.adsDetails.floorspace}
-              onChange={this.handleChange("floorspace")}
-              className={classes.textField}
-              margin="normal"
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <FormControl
-              component="fieldset"
-              style={{ float: "left" }}
-              className={classes.formControl}
-            >
-              <FormLabel component="legend" style={{ textAlign: "left" }}>
-                House type
-              </FormLabel>
-              <RadioGroup
-                row
-                aria-label="housetype"
-                name="housetype"
-                value={this.state.adsDetails.housetype}
-                onChange={this.handleChange("housetype")}
+            <Grid item xs={6}>
+              <TextField
+                id="standard-multiline-flexible"
+                label="Price"
+                fullWidth
+                type="number"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                value={this.state.adsDetails.price}
+                onChange={this.handleChange("price")}
+                className={classes.textField}
+                margin="normal"
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                id="standard-multiline-flexible"
+                label="Floor Space"
+                fullWidth
+                type="number"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                value={this.state.adsDetails.floorspace}
+                onChange={this.handleChange("floorspace")}
+                className={classes.textField}
+                margin="normal"
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <FormControl
+                component="fieldset"
+                style={{ float: "left" }}
+                className={classes.formControl}
               >
-                <FormControlLabel
-                  value="Apartment"
-                  control={<Radio color="primary" />}
-                  label="Apartment"
-                />
-                <FormControlLabel
-                  value="Bungalow"
-                  control={<Radio color="primary" />}
-                  label="Bungalow"
-                />
-              </RadioGroup>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              id="standard-multiline-flexible"
-              label="Location"
-              fullWidth
-              value={this.state.adsDetails.location}
-              onChange={this.handleChange("location")}
-              className={classes.textField}
-              InputLabelProps={{
-                shrink: true,
-              }}
-              margin="normal"
-            />
-          </Grid>
+                <FormLabel component="legend" style={{ textAlign: "left" }}>
+                  House type
+                </FormLabel>
+                <RadioGroup
+                  row
+                  aria-label="housetype"
+                  name="housetype"
+                  value={this.state.adsDetails.housetype}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  onChange={this.handleChange("housetype")}
+                >
+                  <FormControlLabel
+                    value="Apartment"
+                    control={<Radio color="primary" />}
+                    label="Apartment"
+                  />
+                  <FormControlLabel
+                    value="Bungalow"
+                    control={<Radio color="primary" />}
+                    label="Bungalow"
+                  />
+                </RadioGroup>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                id="standard-multiline-flexible"
+                label="Location"
+                fullWidth
+                value={this.state.adsDetails.location}
+                onChange={this.handleChange("location")}
+                className={classes.textField}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                margin="normal"
+              />
+            </Grid>
 
-          {/* <Grid item xs={12}>
-                    <TextField
-                        id="outlined-email-input"
-                        label="Email"
-                        className={classes.textField}
-                        fullWidth
-                        type="email"
-                        name="email"
-                        autoComplete="email"
-                        margin="normal"
-                        variant="outlined"
-                        onChange={this.handleChange('email')}
-                    />
-                    </Grid> */}
-          {/* <Grid item xs={12}>
-                    <TextField
-                        id="outlined-password-input"
-                        label="Password"
-                        className={classes.textField}
-                        type="password"
-                        name="password"
-                        autoComplete="current-password"
-                        margin="normal"
-                        variant="outlined"
-                        onChange={this.handleChange('password')}
-                    />
-                    </Grid> */}
-
-          <Grid item xs={12}>
-            <Button
-              variant="outlined"
-              className={classes.button}
-              onClick={this.updateState}
-            >
-              Save
-            </Button>
+            <Grid item xs={12}>
+              <Button
+                variant="outlined"
+                className={classes.button}
+                onClick={this.updateState}
+              >
+                Save
+              </Button>
+            </Grid>
           </Grid>
-        </Grid>
-      </div>
-      // </Layout>
+        </div>
+      </Fragment>
     );
   }
 }
